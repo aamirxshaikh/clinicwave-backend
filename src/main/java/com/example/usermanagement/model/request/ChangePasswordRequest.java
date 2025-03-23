2f73c8494974b70ec74cecacb4ccbd4197f4e177
aamirxshaikh <aamirshaikh3232@gmail.com>
2025-03-23 22:43:45 +0530

    Add  validation to ChangePasswordRequest

--- src/main/java/com/example/usermanagement/model/request/ChangePasswordRequest.java
+++ src/main/java/com/example/usermanagement/model/request/ChangePasswordRequest.java
@@ -1,11 +1,12 @@
 package com.example.usermanagement.model.request;
 
+import com.example.usermanagement.validation.ValidPassword;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ChangePasswordRequest(
         @NotBlank String currentPassword,
-        @NotBlank @Size(min = 6, max = 40) String newPassword,
+        @NotBlank @Size(min = 6, max = 40) @ValidPassword String newPassword,
         @NotBlank @Size(min = 6, max = 40) String confirmPassword
 ) {
 }
