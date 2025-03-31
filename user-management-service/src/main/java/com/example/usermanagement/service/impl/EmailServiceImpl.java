2d8bde669791b70060e814e3cef5820d60888e45
aamirxshaikh <aamirshaikh3232@gmail.com>
2025-03-31 20:42:14 +0530

    Add RabbitMQ configuration and update EmailServiceImpl to use RabbitTemplate
    This commit introduces the following changes to the codebase:
    
    - Created `RabbitMQConfig` class for RabbitMQ configuration
    - Created `EmailRequest` model class
    - Updated `EmailServiceImpl` to use `RabbitTemplate` for sending messages
    - Removed email sending logic using `JavaMailSender` and `TemplateEngine` from `EmailServiceImpl`

--- user-management-service/src/main/java/com/example/usermanagement/service/impl/EmailServiceImpl.java
+++ user-management-service/src/main/java/com/example/usermanagement/service/impl/EmailServiceImpl.java
@@ -1,45 +1,27 @@
 package com.example.usermanagement.service.impl;
 
-import com.example.usermanagement.exception.EmailSendingException;
+import com.example.usermanagement.model.request.EmailRequest;
 import com.example.usermanagement.service.EmailService;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailServiceImpl implements EmailService {
-  private final JavaMailSender mailSender;
-  private final TemplateEngine templateEngine;
+  private final RabbitTemplate rabbitTemplate;
 
-  @Value("${spring.mail.username}")
-  private String fromEmail;
+  @Value("${rabbitmq.email-verification-queue}")
+  private String emailVerificationQueue;
 
   @Override
   public void sendVerificationEmail(String to, String code) {
-    try {
-      Context context = new Context();
-      context.setVariable("code", code);
-
-      String content = templateEngine.process("verification-email", context);
-
-      MimeMessage message = mailSender.createMimeMessage();
-      MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
-
-      helper.setFrom(fromEmail);
-      helper.setTo(to);
-      helper.setSubject("Verify your email address");
-      helper.setText(content, true);
-
-      mailSender.send(message);
-    } catch (MessagingException e) {
-      throw new EmailSendingException("Failed to send verification email");
-    }
+    log.info("Sending verification email to {}", to);
+    EmailRequest emailRequest = new EmailRequest(to, code);
+    rabbitTemplate.convertAndSend(emailVerificationQueue, emailRequest);
+    log.info("Verification email sent to {}", to);
   }
-} 
+}
