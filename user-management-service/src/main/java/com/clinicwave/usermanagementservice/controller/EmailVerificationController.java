c28d967e80eaf351200ce7e454cd0c4b8eae7e66
aamirxshaikh <aamirshaikh3232@gmail.com>
2025-05-01 22:03:16 +0530

    Remove server.servlet.context-path from application.properties and Add into existing controllers

--- user-management-service/src/main/java/com/clinicwave/usermanagementservice/controller/EmailVerificationController.java
+++ user-management-service/src/main/java/com/clinicwave/usermanagementservice/controller/EmailVerificationController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth/verify")
+@RequestMapping("/api/v1/auth/verify")
 @RequiredArgsConstructor
 public class EmailVerificationController {
   private final EmailVerificationService emailVerificationService;
--- user-management-service/src/main/java/com/clinicwave/usermanagementservice/security/WebSecurityConfig.java
+++ user-management-service/src/main/java/com/clinicwave/usermanagementservice/security/WebSecurityConfig.java
@@ -53,8 +53,8 @@ public class WebSecurityConfig {
             .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth ->
-                    auth.requestMatchers("/auth/**").permitAll()
-                            .requestMatchers("/test/**").permitAll()
+                    auth.requestMatchers("/api/v1/auth/**").permitAll()
+                            .requestMatchers("/api/v1/test/**").permitAll()
                             .anyRequest().authenticated()
             );
 
